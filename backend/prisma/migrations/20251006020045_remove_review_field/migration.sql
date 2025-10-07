2f591f815855332cb71b6e8fcd54072be51a6579
Karim Martin <84159250+karim-martin@users.noreply.github.com>
2025-10-07 04:51:36 +0000

    refactor(migrations): remove obsolete migration files and clean up database schema

--- backend/prisma/migrations/20251006020045_remove_review_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `review` on the `user_ratings` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "user_ratings" DROP COLUMN "review";
